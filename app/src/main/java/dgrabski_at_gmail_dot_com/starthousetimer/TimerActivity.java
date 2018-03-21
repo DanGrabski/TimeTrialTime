83e562106444c7c02f01d602c0ce1d3d265e6aa4
Dan Grabski <dgrabski@gmail.com>
2018-03-21 14:58:42 -0700

    work in progress

--- app/src/main/java/dgrabski_at_gmail_dot_com/starthousetimer/TimerActivity.java
+++ app/src/main/java/dgrabski_at_gmail_dot_com/starthousetimer/TimerActivity.java
@@ -1,6 +1,7 @@
 package dgrabski_at_gmail_dot_com.starthousetimer;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -31,9 +32,10 @@ public class TimerActivity extends AppCompatActivity {
     // ToDo: add different light patterns
 
     int countdownTime;
-    String countdownString;
+    int countdownRemain;
+    String countdownRemainString;
     int tmr_ms, tmr_sec, tmr_min;
-    long time_ms, time_start, time_buff, time_update = 0L;  // time_buff: buffer time for pause
+    long time_ms, time_start, countdown_start, time_buff, time_update = 0L;  // time_buff: buffer time for pause
 
     List<String> lapArrayList;
     String[] lapElements = new String[] {};
@@ -44,6 +46,8 @@ public class TimerActivity extends AppCompatActivity {
     Button start, stop, reset, lap;
     Handler handler;
     Intent intent;
+
+    CountDownTimer cdt_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +68,20 @@ public class TimerActivity extends AppCompatActivity {
         reset = findViewById(R.id.btn_reset);
         lap = findViewById(R.id.btn_lap);
 
+        cdt_start = new CountDownTimer(countdownTime * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                countdownRemain = (int)Math.ceil(millisUntilFinished / 1000.0);
+                countdownView.setText(String.format(Locale.US, "%d", countdownRemain));
+                // ToDo: fire beeps on integral-second intervals
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+
         handler = new Handler();
 
         lapArrayList = new ArrayList<>(Arrays.asList(lapElements));
@@ -73,8 +91,11 @@ public class TimerActivity extends AppCompatActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                time_start = SystemClock.uptimeMillis();
-                handler.postDelayed(runnable, 0);
+                countdown_start = SystemClock.uptimeMillis();
+                time_start = countdown_start + 1000 * countdownTime;
+                cdt_start.start();
+                //handler.postDelayed(timer_countdown, 0);
+                handler.postDelayed(timer_stopwatch, 1000 * countdownTime);
                 reset.setEnabled(false);
                 start.setEnabled(false);
             }
@@ -84,7 +105,10 @@ public class TimerActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //TimeBuff += MillisecondTime;
-                handler.removeCallbacks(runnable);
+                //handler.removeCallbacks(timer_countdown);
+                handler.removeCallbacks(timer_stopwatch);
+                cdt_start.cancel();
+                // ToDo: reset countdown display time
                 reset.setEnabled(true);
                 //start.setEnabled(true);
             }
@@ -120,19 +144,29 @@ public class TimerActivity extends AppCompatActivity {
 
 
 
-    public Runnable runnable = new Runnable() {
+//    public Runnable timer_countdown = new Runnable() {
+//        public void run() {
+//            time_cd_ms = SystemClock.uptimeMillis() - countdown_start;
+//            //time_update = time_buff + time_ms;
+//            tmr_cd_sec = (int) (time_ms / 1000);
+//            tmr_min = tmr_sec / 60;
+//            tmr_sec = tmr_sec % 60;
+//            tmr_ms = (int) (time_ms % 1000);
+//            countdownView.setText(String.format(Locale.US, "%d", tmr_cd_sec));
+//            handler.postDelayed(this, 0);
+//        }
+//    };
 
+    public Runnable timer_stopwatch = new Runnable() {
         public void run() {
-
             time_ms = SystemClock.uptimeMillis() - time_start;
-            time_update = time_buff + time_ms;
-            tmr_sec = (int) (time_update / 1000);
+            //time_update = time_buff + time_ms;
+            tmr_sec = (int) (time_ms / 1000);
             tmr_min = tmr_sec / 60;
             tmr_sec = tmr_sec % 60;
-            tmr_ms = (int) (time_update % 1000);
+            tmr_ms = (int) (time_ms % 1000);
             timeView.setText(String.format(Locale.US, "%02d:%02d.%03d", tmr_min, tmr_sec, tmr_ms));
             handler.postDelayed(this, 0);
         }
-
     };
 }
