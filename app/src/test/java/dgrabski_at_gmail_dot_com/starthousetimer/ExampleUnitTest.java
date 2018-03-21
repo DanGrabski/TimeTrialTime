c5eb0d41fa6edfa4d8a9c7f886b823e70658c87c
Dan Grabski <dgrabski@gmail.com>
2018-03-20 20:53:02 -0700

    initial setup, with structure for stopwatch part of app built

---
+++ app/src/test/java/dgrabski_at_gmail_dot_com/starthousetimer/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package dgrabski_at_gmail_dot_com.starthousetimer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
